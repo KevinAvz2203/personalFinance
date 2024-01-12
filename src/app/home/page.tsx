6ca82f590355dba048dbba927760c589a3a78000
Kevin Valdez <kevinalexisvaldezz@hotmail.com>
2024-01-12 00:39:44 -0700

    Update: Home Page rearrange

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,7 +4,6 @@ import TopCard from "@/components/TopCard";
 import MonthSummary from "@/components/MonthSummary";
 import HistoricActivity from "@/components/HistoricActivity";
 import MonthCashFlow from "@/components/MonthCashFlow";
-import SavingGoals from "@/components/SavingGoals";
 import styles from "./home.module.css";
 import { useSession } from "next-auth/react";
 import Header from "@/components/Header";
@@ -34,19 +33,17 @@ export default function Home() {
         </section>
 
         <section className={styles.genSummary}>
+          {/* Componente grafica Curva summary Month */}
+          <MonthCashFlow User={UserID} />
+
           {/* Componente grafica PIE summary Month */}
           {/* <MonthSummary User={UserID} /> */}
-
-          {/* Componente grafica Curva summary Month */}
-          {/* <MonthCashFlow User={UserID} /> */}
         </section>
 
         <section className={styles.genSummary}>
           {/* Componente grafica PIE summary Month */}
           {/* <HistoricActivity User={UserID} HistoryType={0} /> */}
 
-          {/* Componente grafica Curva summary Month */}
-          {/* <SavingGoals User={UserID} /> */}
         </section>
       </div>
     </>
