65a315be2c0676dbdec5578c72108e15beefb055
Kevin Valdez <kevinalexisvaldezz@hotmail.com>
2024-02-15 16:01:37 -0700

    Update: TopCards component optimization rework

--- src/components/TopCard.tsx
+++ src/components/TopCard.tsx
@@ -4,6 +4,7 @@ import expenseIcon from "/public/assets/icons/expenses-black.png";
 import totalBalanceIcon from "/public/assets/icons/balance-black.png";
 import savedGoalsIcon from "/public/assets/icons/piggy-bank.png";
 import { useState, useEffect } from "react";
+import React from "react";
 import {
   getIncomes,
   getExpenses,
@@ -12,29 +13,14 @@ import {
 import { getTotalSavedGoals } from "@/Backend/Goal";
 import styles from "./TopCard.module.css";
 
-type incomeData = {
-  User: number;
-};
-
-type userSumGoals = {
-  totalSaved: number;
-  totalGoalsAmount: number;
-};
-
-type TotalAmountType = {
-  incomes: number;
-  expenses: number;
-  totalBalance: number;
-};
-
-export default function TopCard({ User }: incomeData) {
-  const [totalAmounts, setTotalAmounts] = useState<TotalAmountType>({
+export default function TopCard({ User }: { User: number }) {
+  const [totalAmounts, setTotalAmounts] = useState({
     incomes: 0,
     expenses: 0,
     totalBalance: 0,
   });
 
-  const [goalsTotalSaved, setGoalsTotalSaved] = useState<userSumGoals>({
+  const [goalsTotalSaved, setGoalsTotalSaved] = useState({
     totalGoalsAmount: 0,
     totalSaved: 0,
   });
@@ -64,65 +50,49 @@ export default function TopCard({ User }: incomeData) {
   return (
     <>
       <div className={styles.container}>
-        <div className={`${styles.topCards}`}>
-          <Image src={incomeIcon} alt="Income Icon" width={50} height={50} />
-          <div>
-            <p>${totalAmounts.incomes | 0} MXN</p>
-            <p>Incomes</p>
-          </div>
-        </div>
-        <span
-          style={{
-            height: 60,
-            border: "1px #8DA9C4 solid",
-          }}
-        />
-        <div className={`${styles.topCards}`}>
-          <Image src={expenseIcon} alt="Expense Icon" width={50} height={50} />
-          <div>
-            <p>${totalAmounts.expenses | 0} MXN</p>
-            <p>Expenses</p>
-          </div>
-        </div>
-        <span
-          style={{
-            height: 60,
-            border: "1px #8DA9C4 solid",
-          }}
-        />
-        <div className={`${styles.topCards}`}>
-          <Image
-            src={totalBalanceIcon}
-            alt="TotalBalance Icon"
-            width={50}
-            height={50}
-          />
-          <div>
-            <p>${totalAmounts.totalBalance | 0} MXN</p>
-            <p>Total Balance</p>
-          </div>
-        </div>
-        <span
-          style={{
-            height: 60,
-            border: "1px #8DA9C4 solid",
-          }}
-        />
-        <div className={`${styles.topCards}`}>
-          <Image
-            src={savedGoalsIcon}
-            alt="savedGoals Icon"
-            width={50}
-            height={50}
-          />
-          <div>
-            <p>
-              ${goalsTotalSaved.totalSaved || 0} of $
-              {goalsTotalSaved.totalGoalsAmount || 0}
-            </p>
-            <p>Total Savings - MXN</p>
-          </div>
-        </div>
+        {[
+          { icon: incomeIcon, amount: totalAmounts.incomes, label: "Incomes" },
+          {
+            icon: expenseIcon,
+            amount: totalAmounts.expenses,
+            label: "Expenses",
+          },
+          {
+            icon: totalBalanceIcon,
+            amount: totalAmounts.totalBalance,
+            label: "Total Balance",
+          },
+          {
+            icon: savedGoalsIcon,
+            amount: goalsTotalSaved.totalSaved,
+            total: goalsTotalSaved.totalGoalsAmount,
+            label: "Total Savings - MXN",
+          },
+        ].map(({ icon, amount, total, label }, index) => (
+          <React.Fragment key={index}>
+            <div className={`${styles.topCards}`}>
+              <Image src={icon} alt={`${label} Icon`} width={50} height={50} />
+              <div>
+                {icon === savedGoalsIcon ? (
+                  <p>
+                    ${amount || 0} of ${total || 0}
+                  </p>
+                ) : (
+                  <p>${amount || 0} MXN</p>
+                )}
+                <p>{label}</p>
+              </div>
+            </div>
+            {index !== 3 && (
+              <span
+                style={{
+                  height: 60,
+                  border: "1px #8DA9C4 solid",
+                }}
+              />
+            )}
+          </React.Fragment>
+        ))}
       </div>
     </>
   );
